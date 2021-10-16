463f72a07154ff3caadddf650bebc18f9a9d6947
Pawan Kumar <pawan@acelrtech.com>
2021-10-16 19:53:07 +0530

    Validations required issue fixed

--- src/main/scala/com/bootes/validators/Validators.scala
+++ src/main/scala/com/bootes/validators/Validators.scala
@@ -48,43 +48,74 @@ object Validators {
 123.456.7890
 +91 (123) 456-7890
    */
-  def validatePhone(value: String):Validation[String, Phone] = matchesRegex("""^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$""").test(value) match {
+  def validatePhone(value: String, required: Option[Boolean] = None):Validation[String, Phone] = matchesRegex("""^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$""").test(value) match {
     case true => Validation.succeed(Phone(value))
-    case false => Validation.fail(s"Phone value seems to be invalid, $value")
+    case false =>
+      required match {
+        case Some(true) =>
+          Validation.fail(s"Phone value seems to be invalid, $value")
+        case _ =>
+          Validation.succeed(Phone(value))
+      }
   }
 
-  def validateEmail(value: String):Validation[String, Email] = emailValidator.isValid(value) match {
+  def validateEmail(value: String, required: Option[Boolean] = None):Validation[String, Email] = emailValidator.isValid(value) match {
     case true => Validation.succeed(Email(value))
-    case false => Validation.fail(s"Email value seems to be invalid, $value")
+    case false =>
+      required match {
+        case Some(true) =>
+          Validation.fail(s"Email value seems to be invalid, $value")
+        case _ => Validation.succeed(Email(value))
+      }
   }
 
-  def validateName(value: String):Validation[String, String] = isNonEmptyString.test(value) && (matchesRegex("""^[-'a-zA-ZÀ-ÖØ-öø-ſ]+$""").test(value)) match {
+  def validateName(value: String, required: Option[Boolean] = None):Validation[String, String] = isNonEmptyString.test(value) && (matchesRegex("""^[-'a-zA-ZÀ-ÖØ-öø-ſ]+$""").test(value)) match {
     case true => Validation.succeed(value)
-    case false => Validation.fail(s"Name value seems to be invalid, $value")
+    case false =>
+      required match {
+        case Some(true) =>
+          Validation.fail(s"Name value seems to be invalid, $value")
+        case _ => Validation.succeed(value)
+      }
   }
 
-  def validateToken(value: String):Validation[String, Token] = Validation.fromPredicateWith(s"Token value seems to be invalid, $value")(Token(value))(token => isNonEmptyString.test(value) && (isGreaterThan(1000) && isLessThan(2000)).test(value.size))
+  def validateToken(value: String, required: Option[Boolean] = None):Validation[String, Token] = required match {
+    case Some(true) =>
+      Validation.fromPredicateWith(s"Token value seems to be invalid, $value")(Token(value))(token => isNonEmptyString.test(value) && (isGreaterThan(1000) && isLessThan(2000)).test(value.size))
+    case _ =>
+      Validation.succeed(Token(value))
+  }
 
-  def validateTitle(value: String):Validation[String, String] = Validation.fromPredicateWith(s"Title cannot be empty, $value")(value)(token => isNonEmptyString.test(value) && (isGreaterThan(2) && isLessThan(200)).test(value.size))
+  def validateTitle(value: String, required: Option[Boolean] = None):Validation[String, String] =
+    required match {
+      case Some(true) =>
+        Validation.fromPredicateWith(s"Title cannot be empty, $value")(value)(token => isNonEmptyString.test(value) && (isGreaterThan(2) && isLessThan(200)).test(value.size))
+      case _ => Validation.succeed(value)
+    }
 
-  def validateValidationsElement(values: Seq[String], value: Validations):Validation[String, Validations] = {
-
+  def validateValidationsElement(values: Seq[String], value: Validations, required: Option[Boolean] = None):Validation[String, Validations] = {
+    //println(s"validateValidationsElement: values = $values with required = $required")
     def check[T](values: Seq[String], predicate: String => Boolean): (Boolean, String) = {
-      var allErrors: scala.collection.mutable.Buffer[String] = scala.collection.mutable.Buffer.empty
-      val (newVals: Seq[String], vals: Seq[String], originalSize: Int) =  (values.filter(x => {
-        val cond = predicate(x)
-        if (!cond) {
-          allErrors = allErrors :+ x
-        }
-        //println(s"Predicate result = $cond for x = $x and values = $values")
-        cond
-      }), values, values.size)
-      //println(s"newVals = $newVals, vals = $vals and size = $originalSize")
-      (values.nonEmpty && (newVals.size == originalSize), if (allErrors.isEmpty) "" else "(" + allErrors.mkString(", ") + ")")
+      required match {
+        case Some(true) =>
+          var allErrors: scala.collection.mutable.Buffer[String] = scala.collection.mutable.Buffer.empty
+          val (newVals: Seq[String], vals: Seq[String], originalSize: Int) =  (values.filter(x => {
+            val cond = predicate(x)
+            if (!cond) {
+              allErrors = allErrors :+ x
+            }
+            //println(s"Predicate result = $cond for x = $x and values = $values")
+            cond
+          }), values, values.size)
+          //println(s"newVals = $newVals, vals = $vals and size = $originalSize")
+          (values.nonEmpty && (newVals.size == originalSize), if (allErrors.isEmpty) "" else "(" + allErrors.mkString(", ") + ")")
+        case _ =>
+          (true, "")
+      }
+
     }
 
     def makeDisplayableValue(value: String) = if (value.isEmpty) "<empty>" else value
-
     value.`type` match {
       case "int" | "number" | "integer" | "Int" | "Integer" =>
         def validateInt(x: String): Boolean = {
@@ -132,6 +163,7 @@ object Validators {
             true
         }
         val result = check(values, validateRegex)
+        //println(s"regex: result = $result")
         if (result._1) Validation.succeed(value) else Validation.fail(s"`type`, regex mismatch for the values, ${makeDisplayableValue(result._2)} provided")
       case _ =>
         Validation.succeed(value)
@@ -140,16 +172,16 @@ object Validators {
   def validateFormSection(value: FormSection):Validation[FormSection, FormSection] = {
       var allErrors: scala.collection.mutable.Buffer[String] = scala.collection.mutable.Buffer.empty
       val elements: Seq[CreateElementRequest] = value.elements.map(e => {
-        val title = validateTitle(e.name).toEither
-        val sectionName = validateTitle(e.sectionName.getOrElse("")).toEither
-        val chkType = validateTitle(e.`type`).toEither
+        val title = validateTitle(e.name, e.required).toEither
+        val sectionName = validateTitle(e.sectionName.getOrElse(""), e.required).toEither
+        val chkType = validateTitle(e.`type`, e.required).toEither
         val result: Seq[Either[NonEmptyChunk[String], Validations]] = e.`type` match {
           case "select" | "multiselect" | "Select" | "Multiselect" =>
             val opts = e.options.getOrElse(Seq.empty)
             val values = opts.map(x => x.value)
-            e.validations.map(validator => validateValidationsElement(values, validator).toEither)
+            e.validations.map(validator => validateValidationsElement(values, validator, e.required).toEither)
           case _ =>
-            e.validations.map(validator => validateValidationsElement(e.values, validator).toEither)
+            e.validations.map(validator => validateValidationsElement(e.values, validator, e.required).toEither)
         }
         val filteredResults: Seq[Either[NonEmptyChunk[String], Validations]] = result.filter(r => r.isLeft)
         val errors: Seq[String] = Seq(title, sectionName, chkType).filter(r => {
